1af94a7cdf7713550d9eddf78942ef1428df4f6b
Edwin van Leeuwen <edder@tkwsping.nl>
2009-12-03 14:07:47 +0100

    Clean up header file

--- include/cairo_plot.h
+++ include/cairo_plot.h
@@ -30,34 +30,3 @@ namespace cairo_plot {
     };
 }
 #endif
-
-/*#include <X11/Xlib.h>
-#include <boost/thread.hpp>
-#include <boost/bind.hpp>
-#include <cairomm/context.h>
-#include <cairomm/xlib_surface.h>
-#ifndef CAIRO_PLOT_H
-#define CAIRO_PLOT_H
-
-namespace cairo_plot {
-	
-	 Class controls the plotting
-	
-	class Plot {
-		public:
-            // Should open cairo surface
-            Plot( float max_x, float max_y );
-            
-            ~Plot();
-			// Plots a point to the surface
-			void plot_point( float x, float y );
-        private:
-			Cairo::RefPtr<Cairo::XlibSurface> pSurface;
-			Cairo::RefPtr<Cairo::Context> pContext;
-			boost::thread event_thrd;
-		    Display *dpy;
-
-            void event_loop();
-	};
-}
-#endif*/
