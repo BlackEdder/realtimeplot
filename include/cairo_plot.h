3b090d15f1fda383235b22a3ae8f5d3db2d3da8d
Edwin van Leeuwen <edder@tkwsping.nl>
2009-12-03 14:06:34 +0100

    Plot draws to an image surface and displays this surface every second

--- include/cairo_plot.h
+++ include/cairo_plot.h
@@ -1,27 +1,33 @@
-#include <cairomm/context.h>
-#include <cairomm/xlib_surface.h>
-#include <boost/thread.hpp>
-#include <boost/bind.hpp>
 #ifndef CAIRO_PLOT_H
 #define CAIRO_PLOT_H
+
+#include <cairomm/context.h>
+#include <cairomm/xlib_surface.h>
+
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+
 
 namespace cairo_plot {
 	/*
 	 * Class controls the plotting
 	 */
 	class Plot {
-		public:
-			Cairo::RefPtr<Cairo::XlibSurface> pSurface;
-			Cairo::RefPtr<Cairo::Context> pContext;
-            Display *dpy;
+        public:
+            Cairo::RefPtr<Cairo::ImageSurface> surface;
+            Cairo::RefPtr<Cairo::Context> context;
             boost::shared_ptr<boost::thread> pEvent_thrd;
-			// Should open cairo surface
-			Plot( float max_x, float max_y );
+
+            int width;
+            int height;
+
+            // Should open cairo surface
+            Plot( int max_x, int max_y );
             ~Plot();
-			// Plots a point to the surface
-			void plot_point( float x, float y );
+            // Plots a point to the surface
+            void plot_point( float x, float y );
             void event_loop();
-	};
+    };
 }
 #endif
 
--- src/cairo_plot.cc
+++ src/cairo_plot.cc
@@ -1,17 +1,31 @@
 #include "cairo_plot.h"
 
-#include<iostream>
-
 namespace cairo_plot {
 
-    Plot::Plot( float max_x, float max_y ) {
-		
-		//Snap dit niet helemaal
-		Window rootwin, win;
-		int scr, white, black;
+    Plot::Plot( int x_size, int y_size ) {
+        width = x_size;
+        height = y_size;
+        
+        surface = Cairo::ImageSurface::create(Cairo::FORMAT_ARGB32, width, height);
+        context = Cairo::Context::create(surface); 
 
-		if(!(dpy=XOpenDisplay(NULL))) {
-			fprintf(stderr, "ERROR: Could not open display\n");
+        pEvent_thrd = boost::shared_ptr<boost::thread>( new boost::thread( boost::bind( &cairo_plot::Plot::event_loop, this ) ) );
+    }
+
+    Plot::~Plot() { 
+        pEvent_thrd->join(); 
+    }
+
+    void Plot::event_loop() {
+        Window rootwin;
+        int scr, white, black;
+        Cairo::RefPtr<Cairo::XlibSurface> xSurface;
+        Cairo::RefPtr<Cairo::Context> xContext;
+        Display *dpy;
+        Window win;
+
+        if(!(dpy=XOpenDisplay(NULL))) {
+            fprintf(stderr, "ERROR: Could not open display\n");
             throw;
         }
 
@@ -22,46 +36,33 @@ namespace cairo_plot {
         win = XCreateSimpleWindow(dpy,
                 rootwin,
                 0, 0,   // origin
-                100, 100, // size
+                width, height, // size
                 0, black, // border
                 white );
         XStoreName(dpy, win, "hello");
-		XMapWindow(dpy, win);
-		//nu snap ik het weer. Maar bovenstaande kan mooier
+        XMapWindow(dpy, win);
+        //nu snap ik het weer. Maar bovenstaande kan mooier
 
+        xSurface = Cairo::XlibSurface::create( dpy, win , DefaultVisual(dpy, 0), width, height);
+        xContext = Cairo::Context::create( xSurface );
+        std::cout << "debug " << std::endl;
 
-		pSurface = Cairo::XlibSurface::create( dpy, win , DefaultVisual(dpy, 0), 100, 100);
-		pContext = Cairo::Context::create( pSurface );
-
-        pEvent_thrd = boost::shared_ptr<boost::thread>( new boost::thread( boost::bind( &cairo_plot::Plot::event_loop, this ) ) );
-
-		pContext->save(); // save the state of the context
-        pContext->set_source_rgb(0.86, 0.85, 0.47);
-        pContext->paint();    // fill image with the color
-        pContext->restore();  // color is back to black now
-    }
-
-    Plot::~Plot() { 
-        pEvent_thrd->join(); 
-    }
-
-    void Plot::event_loop() {
-        XEvent e;
+        xContext->set_source( surface, 0, 0 );
+        std::cout << "debug 1" << std::endl;
+        
         while(1) {
-            XNextEvent(dpy, &e);
-            if(e.type==Expose && e.xexpose.count<1) {
-            } else if(e.type==ButtonPress) break;
+            xContext->paint();
+            sleep(1);
         }
 
     }
 
 	void Plot::plot_point( float x, float y ) {
-		pContext->set_line_width(20.0);
-		pContext->set_source_rgb(0.86, 0.85, 0.47);
-		pContext->rectangle( 0.0, 0.0, pSurface->get_width(), pSurface->get_height());
-		pContext->stroke();
-		pContext->set_source_rgb(0, 0, 0);
-		pContext->fill();
-       
+		context->set_line_width(20.0);
+		context->set_source_rgb(0.86, 0.85, 0.47);
+		context->rectangle( 0.0, 0.0, surface->get_width(), surface->get_height());
+		context->stroke();
+		context->set_source_rgb(0, 0, 0);
+		context->fill();
 	}
 }
