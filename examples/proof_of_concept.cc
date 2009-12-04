01177804bd6c05fad5ad54e79947e5d7d6ca91cc
Edwin <edwin@tkwsping.nl>
2009-12-04 22:16:56 +0100

    Proof of concept for xaxis using surface

--- examples/proof_of_concept.cc
+++ examples/proof_of_concept.cc
@@ -14,6 +14,9 @@
 
 Cairo::RefPtr<Cairo::ImageSurface> surface;
 Cairo::RefPtr<Cairo::Context> context;
+Cairo::RefPtr<Cairo::ImageSurface> xaxis_surface;
+Cairo::RefPtr<Cairo::Context> xaxis_context;
+
 
 float min_x = -10;
 float max_x = 40;
@@ -51,25 +54,49 @@ void expose_pixmap() {
     pSurface = Cairo::XlibSurface::create( dpy, win , DefaultVisual(dpy, 0), 200, 200);
     pContext = Cairo::Context::create( pSurface );
 
-    while(1) {
-        pContext->set_source( surface, 0, 0 );
-        pContext->paint();
-        sleep(1);
-    }
+		while(1) {
+			//this seems wrong order to me, but it works this way and not in reverse
+			pContext->rectangle(20,0, pSurface->get_width(), pSurface->get_height());
+			pContext->clip();
+			pContext->set_source( xaxis_surface, 0, pSurface->get_height()-28 );
+			pContext->paint();
+			pContext->reset_clip();
+			pContext->rectangle(20,0, pSurface->get_width(), pSurface->get_height()-20);
+			pContext->clip(  );
+			pContext->set_source( surface, 0, 0 );
+			pContext->paint();
+			pContext->reset_clip();
+			sleep(1);
+		}
 }
 
 void draw_point(float x, float y) {
     double dx = 1;
     double dy = 1;
     context->device_to_user_distance(dx,dy);
-    //not sure what is going wrong, dx and dy are translated correctly
+    //not really working due to line_width issues
     context->rectangle(x,y,dx,dy);
 }
 
 int main() {
     surface = Cairo::ImageSurface::create(Cairo::FORMAT_ARGB32, 200, 200);
     context = Cairo::Context::create(surface);
-    
+    xaxis_surface = Cairo::ImageSurface::create(Cairo::FORMAT_ARGB32, 200, 28);
+    xaxis_context = Cairo::Context::create(xaxis_surface);
+		xaxis_context->scale( xaxis_surface->get_width()/(max_x-min_x), 1 );
+    xaxis_context->translate( -min_x, 0 );
+		xaxis_context->move_to(0,0);
+		xaxis_context->line_to(0,8);
+		xaxis_context->move_to(min_x,8);
+		xaxis_context->line_to(max_x,8);
+		xaxis_context->stroke();
+ 
+     
+    //set white background
+		context->set_source_rgb(1,1,1);
+    context->rectangle(0,0,surface->get_width(),surface->get_height());
+    context->fill();
+
     context->set_source_rgb(0.5,0.5,0);
     context->rectangle(0,0,surface->get_width(),surface->get_height());
     context->stroke();
@@ -81,9 +108,16 @@ int main() {
     
     context->set_source_rgb(0,0,0);
     draw_point(0,0);
-    draw_point(max_x, max_y);
-    draw_point(min_x, min_y);
+    draw_point(2,2);
+    draw_point(max_x-1, max_y-1);
+    draw_point(min_x+1, min_y+1);
     context->stroke();
+		context->clip();
+    draw_point(-5,-5);
+    draw_point(5,5);
+    context->stroke();
+
+		//now work on the clipping
 
     boost::thread thrd( &expose_pixmap );
     thrd.join();
