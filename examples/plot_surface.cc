93ae60727e2fd99541505e7af6d93cc190af7458
BlackEdder <edwin@tkwsping.nl>
2010-11-01 17:20:26 +0000

    Implemented example to draw surface plots

---
+++ examples/plot_surface.cc
@@ -0,0 +1,53 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2010, Edwin van Leeuwen
+  
+  This file is part of RealTimePlot.
+  
+  RealTimePlot is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  RealTimePlot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with RealTimePlot. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <cmath>
+
+#include "realtimeplot/plot.h"
+
+using namespace realtimeplot;
+
+inline double rnorm( double mean, double sigma ) {
+	double x1, x2, w;
+	do {
+		double rnd1 = double(rand())/RAND_MAX;
+		double rnd2 = double(rand())/RAND_MAX;
+		x1 = 2.0 * rnd1 - 1.0;
+		x2 = 2.0 * rnd2 - 1.0;
+		w = x1 * x1 + x2 * x2;
+	} while ( w >= 1.0 || w == 0 );
+
+	w = sqrt( (-2.0 * log( w ) ) / w );
+	return mean + sigma * x1 * w;
+}
+
+int main() {
+	SurfacePlot pl = SurfacePlot( -1, 1, -1, 1 );
+	for( size_t i=0; i<1000; ++i ) {
+		pl.point( rnorm( 0, 0.1 ), rnorm( 0, 0.2 ) );
+		/*std::cout << rnorm( 0, 0.1 ) << std::endl;
+		std::cout << rnorm( 0, 0.2 ) << std::endl;*/
+	}
+	return 0;
+}
