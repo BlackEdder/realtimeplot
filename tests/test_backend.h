cee17472f34f8004b332234ff225b7c47e15873e
BlackEdder <edwin@tkwsping.nl>
2011-01-25 09:39:46 +0000

    Some more tests for gradientVector

--- tests/test_backend.h
+++ tests/test_backend.h
@@ -33,6 +33,18 @@ class TestBackend : public CxxTest::TestSuite
 			TS_ASSERT_EQUALS( v[1]->x, 1.5 );
 			TS_ASSERT_EQUALS( v[1]->y, 0.5 );
 			TS_ASSERT_EQUALS( v[1]->z, 1 );
+
+			Triangle3D tr2 = Triangle3D();
+			tr2.vertices.push_back( pV3 );
+			tr2.vertices.push_back( pV1 );
+			tr2.vertices.push_back( pV2 );
+			v = tr2.gradientVector();
+			TS_ASSERT_EQUALS( v[0]->x, 1 );
+			TS_ASSERT_EQUALS( v[0]->y, 0 );
+			TS_ASSERT_EQUALS( v[0]->z, 0 );
+			TS_ASSERT_EQUALS( v[1]->x, 1.5 );
+			TS_ASSERT_EQUALS( v[1]->y, 0.5 );
+			TS_ASSERT_EQUALS( v[1]->z, 1 );
 		}
 };
 	
