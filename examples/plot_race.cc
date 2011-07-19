d0c376be22832f2ddaf88363adb55dcd65008cbd
BlackEdder <edwin@tkwsping.nl>
2011-07-19 14:04:32 +0100

    Example that shows one bug, probably due to race condition

---
+++ examples/plot_race.cc
@@ -0,0 +1,51 @@
+/*
+	 -------------------------------------------------------------------
+
+	 Copyright (C) 2010, Edwin van Leeuwen
+
+	 This file is part of RealTimePlot.
+
+	 RealTimePlot is free software; you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation; either version 3 of the License, or
+	 (at your option) any later version.
+
+	 RealTimePlot is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with RealTimePlot. If not, see <http://www.gnu.org/licenses/>.
+
+	 -------------------------------------------------------------------
+	 */
+
+/*
+ * Testing for crashes (due to threading mostly)
+ */
+
+#include "realtimeplot/plot.h"
+
+using namespace realtimeplot;
+
+int main() {
+	PlotConfig config = PlotConfig();
+	config.min_x = -0.111;
+	config.max_x = 1.04;
+	config.min_y = -20;
+	config.max_y = 30;
+	//config.margin_x = 200;
+	//config.font = "sans 12";
+	config.aspect_ratio = 0.5;
+	config.point_size = 2;
+	config.xlabel = "Very Long Long label";
+	Plot pl = Plot();
+	pl.reset( config );
+	sleep( 1 );
+	Plot pl2 = Plot();
+	// This plot does not get shown unless we sleep first
+	//sleep( 1 );
+	pl2.reset( config );
+	return 0;
+}
