5c152b9e6afee115fbacb77bd5f12b1a3127da4a
BlackEdder <edwin@tkwsping.nl>
2011-08-03 17:36:18 +0100

    Framework for new xcb_eventhandler
    This eventhandler will be a singular class that maintains the connection
    with x and handles events. In case of x events it will need to send them
    to the correct plots.

---
+++ tests/test_xcb_eventhandler.h
@@ -0,0 +1,25 @@
+#include <cxxtest/TestSuite.h>
+
+#include "realtimeplot/xcb_eventhandler.h"
+
+using namespace realtimeplot;
+
+class TestXcbEventHandler : public CxxTest::TestSuite 
+{
+	public:
+		/*
+		 * Make sure we can only create one object
+		 */
+		void testSingularity() {}
+
+		/*
+		 * x connection
+		 */
+		void testXcbConnection() {}
+
+		/*
+		 * Opening windows
+		 */
+		void testXcbOpenWindow() {}
+};
+	
