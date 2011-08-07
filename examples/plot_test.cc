097ee7ae99ae2b155c3044fa06ee54cbf6d6e791
BlackEdder <edder@tkwsping.nl>
2011-08-07 13:28:15 +0100

    Add examples/plot_test.cc

---
+++ examples/plot_test.cc
@@ -0,0 +1,45 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2010, Edwin van Leeuwen
+  
+  This file is part of RealTimePlot.
+  
+  RealTimePlot is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  RealTimePlot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with RealTimePlot. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+
+#include "realtimeplot/plot.h"
+
+using namespace realtimeplot;
+
+int main() {
+	PlotConfig config = PlotConfig();
+	config.min_x = -10;
+	config.max_x = 90;
+	config.min_y = -20;
+	config.max_y = 30;
+
+	Plot pl = Plot();
+	pl.reset( config );
+	Plot pl1 = Plot();
+	pl1.reset( config );
+	Plot pl2 = Plot();
+	pl2.reset( config );
+	Plot pl3 = Plot();
+	pl3.reset( config );
+
+	return 0;
+}
