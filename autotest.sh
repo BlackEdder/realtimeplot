4e5caa81dd0ecfdda245b1a2ac1e1ad01741269b
BlackEdder <edwin@tkwsping.nl>
2012-06-22 10:29:59 +0100

    Listen for ATTRIB instead of CREATE events in autotest.sh
    Should stop the test being run before the whole file is written

--- autotest.sh
+++ autotest.sh
@@ -4,7 +4,8 @@ ctest
 
 while : 
 do
-	file=`inotifywait -q -e CREATE bin/ --format %f`
+	#file=`inotifywait -q -e CREATE bin/ --format %f`
+	file=`inotifywait -q -e ATTRIB bin/ --format %f`
 	if [[ "$file" =~ unittest_* ]]
 	then
 		sleep 2
